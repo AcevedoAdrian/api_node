aa0cb1d3302409817d8c6c3cccce0ce3a0b66877
AcevedoAdrian <acevedohugoadrian@gmail.com>
2022-05-04 07:20:25 -0300

    Inicializacion de un proyecto para crear un api en node js

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express'); // Import express
+const cors = require('cors'); // Import cors
+
+
+const app = express(); // Create express app
+app.use(cors()); // Enable cors, 
+
+const port = process.env.PORT || 3000;  // Set port from environment variable or 3000
+
+app.listen(port, () =>{
+  console.log(`Server is running on port https://localhost:${port}`);
+}) // Start serve, escuhca por el puerto 3000
